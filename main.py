12cb95f67760d306c9cde0c6e163a5931744837d
Juan Sanquintin <jsanquintin@hotmail.com>
2025-07-08 17:21:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # Usuario fijo (clave123)
 fake_user = {
     "username": "admin",
-    "hashed_password": "$2a$12$ySnu7TMYP8IKn/OFUBOCC.LUV/4IciZZf/ZsZBsewEUHZ5eNSug3K"
+    "hashed_password": $2b$12$MI3pMJpQmvP/6g7rhO78q.AKrAEsQeu7QUJxBRuLRnmkj95Q4ebHy"
 }
 
 def verify_password(plain_password, hashed_password):
